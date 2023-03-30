366fc174b77e134b88cf50d87cb9521d7640cfc0
Dimitri Andre <dimitri@ymagyn.com>
2023-03-30 10:49:28 +0200

    create header and footer components

--- src/index.js
+++ src/index.js
@@ -2,4 +2,32 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<React.StrictMode></React.StrictMode>);
+
+function HeaderContent() {
+  return <h1>REACT COURSE</h1>;
+}
+
+function MainBody() {
+  return (
+    <div>
+      <p>In this course, we will learn react js by building TaskOPedia!</p>
+      <ul>
+        <li>Call Ben</li>
+        <li>Go to Walmart</li>
+      </ul>
+      <p>Happy coding</p>
+    </div>
+  );
+}
+
+function FooterContent() {
+  return <p>Happy coding</p>;
+}
+
+root.render(
+  <div>
+    <HeaderContent />
+    <MainBody />
+    <FooterContent />
+  </div>
+);
